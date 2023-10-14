aaad686ed5813e83ca906ce2fd11b663a59e59da
rifla93 <ralfsemi@yahoo.com>
2023-10-14 03:55:45 -0700

    101-quote.c

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,8 +1,7 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 
 int main(void)
 {
-    fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-    return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
