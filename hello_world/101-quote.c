4928b95417d355d3ee2d3574ade4988c1934ec3e
rifla93 <ralfsemi@yahoo.com>
2023-10-14 04:02:54 -0700

    101-quote.c

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,5 +1,11 @@
 #include <unistd.h>
-
+/**
+ * main - Entry point
+ *
+ * Description: Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19"
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
