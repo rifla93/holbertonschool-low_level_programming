39bb72eeeb1c2a19ec121ba49cc8ef79ad1e52f7
rifla93 <ralfsemi@yahoo.com>
2023-10-17 05:54:29 -0700

    8-print_base16.c

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	char b;
+
+	for (i = 0; i < 10; i++)
+		putchar (i + '0');
+	for (b = 'a'; b <= 'f'; b++)
+		putchar (b);
+	putchar('\n');
+	return (0);
+}
