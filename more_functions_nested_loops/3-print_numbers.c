f63a85c83482820c9005cf9ef8b73115493c9866
rifla93 <ralfsemi@yahoo.com>
2023-10-20 02:23:00 -0700

    0-isupper

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,12 @@
+#include "main.h"
+void print_numbers(void)
+{
+    int i;
+
+    for (i = 48; i < 58; i++)
+    {
+        _putchar(i);
+    }
+    _putchar('\n');
+}
+
---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+void print_most_numbers(void)
+{
+int number;
+	for (number = 0; number < 10; number++)
+	{
+	
+	if (number != 2 && number !=4)
+	{
+		_putchar(number + '0');
+	}
+	}
+
+		_putchar('\n');
+
+
+
+
+}
