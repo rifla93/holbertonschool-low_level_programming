d6f1e7849722dc243132b1e884423ee334d18ed0
rifla93 <ralfsemi@yahoo.com>
2023-10-20 02:46:31 -0700

    3-print_numbers

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,12 +1,16 @@
 #include "main.h"
+/**
+ * print_numbers - prints the numbers
+ * Return: 0
+ */
 void print_numbers(void)
 {
-    int i;
+	int i;
 
-    for (i = 48; i < 58; i++)
-    {
-        _putchar(i);
-    }
-    _putchar('\n');
+	for (i = 48; i < 58; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
 }
 
