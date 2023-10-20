cc795b0e5f9d31ddda3da0b266b573435befda49
rifla93 <ralfsemi@yahoo.com>
2023-10-20 02:50:55 -0700

    4-print_most_numbers

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,19 +1,21 @@
 #include "main.h"
+/**
+ * print_most_numbers - prints the numbers
+ * Return: 0
+ */
+
 void print_most_numbers(void)
 {
-int number;
-	for (number = 0; number < 10; number++)
+	int number;
+
+		for (number = 0; number < 10; number++)
 	{
-	
-	if (number != 2 && number !=4)
+		if (number != 2 && number != 4)
 	{
 		_putchar(number + '0');
 	}
 	}
 
-		_putchar('\n');
-
-
-
+		_putchar('\n')i;
 
 }
