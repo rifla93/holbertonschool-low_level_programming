0b5f37748e9884a0030f1bf696302dc564c04f8b
rifla93 <ralfsemi@yahoo.com>
2023-11-13 04:24:17 -0800

    2-print_dog.c

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * print_dog - Short description
+ * @d: d
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		printf("Name: %s\n", d->name ? d->name : "(nil)");
+		printf("Age: %.6f\n", d->age);
+		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+	}
+}
