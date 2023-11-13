a2916ae47ace0140e6d44fab75396a8e3f4f7b6f
rifla93 <ralfsemi@yahoo.com>
2023-11-13 02:33:12 -0800

    1-init_dog.c

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+/**
+ * init_dog - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ * @d: d
+ * Description: Longer description
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
